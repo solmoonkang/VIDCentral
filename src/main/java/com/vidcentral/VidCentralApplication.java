7a16ca37d6695b547be9a9339c7d99de7d248df0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-20 22:44:04 +0900

    feat: 스프링 설정 프로퍼티 활성화로 토큰 설정 클래스 등록

--- src/main/java/com/vidcentral/VidCentralApplication.java
+++ src/main/java/com/vidcentral/VidCentralApplication.java
@@ -2,9 +2,13 @@ package com.vidcentral;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.vidcentral.global.config.TokenConfig;
+
 @EnableJpaAuditing
+@EnableConfigurationProperties(TokenConfig.class)
 @SpringBootApplication
 public class VidCentralApplication {
 
