fedcda492f747b954c9e36002dc0101fad6be5ba
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-20 19:19:05 +0900

    feat: Member 모델 구현

---
+++ src/main/java/com/vidcentral/api/domain/entity/Member.java
@@ -0,0 +1,46 @@
+package com.vidcentral.api.domain.entity;
+
+import com.vidcentral.global.common.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "MEMBER")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long memberId;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "nickname", nullable = false, unique = true)
+	private String nickname;
+
+	@Column(name = "profile_image", nullable = false)
+	private String profileImage;
+
+	@Builder
+	private Member(String email, String password, String nickname, String profileImage) {
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.profileImage = profileImage;
+	}
+}
