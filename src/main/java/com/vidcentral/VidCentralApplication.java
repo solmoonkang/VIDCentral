c9c32fae6e9087e25126007e3ba8986c1e79e113
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-20 16:31:34 +0900

    초기 세팅

---
+++ src/main/java/com/vidcentral/VidCentralApplication.java
@@ -0,0 +1,13 @@
+package com.vidcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidcentral/VidCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
