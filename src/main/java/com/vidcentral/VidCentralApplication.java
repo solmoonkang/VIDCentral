a319c0fb9437e4fa9157a72fbbf45efc2b252018
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-20 18:57:42 +0900

    feat: 애플리케이션에서 JPA 감사 기능 활성화

--- src/main/java/com/vidcentral/VidCentralApplication.java
+++ src/main/java/com/vidcentral/VidCentralApplication.java
@@ -2,7 +2,9 @@ package com.vidcentral;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class VidCentralApplication {
 
