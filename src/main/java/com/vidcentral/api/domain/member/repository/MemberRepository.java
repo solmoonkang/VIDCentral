3172d6c9b4b3b7fdce46a9754f9a8e6205b1036a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-20 21:08:29 +0900

    feat: 회원 관련 기능을 위한 인터페이스 추가

--- src/main/java/com/vidcentral/api/domain/member/repository/MemberRepository.java
+++ src/main/java/com/vidcentral/api/domain/member/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.vidcentral.api.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,10 @@ import com.vidcentral.api.domain.member.entity.Member;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findMemberByEmail(String email);
+
+	boolean existsMemberByEmail(String email);
+
+	boolean existsMemberByNickname(String nickname);
 }
