c5ae80888f6bffa3542de6aea9e434efaad3edd0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 16:22:29 +0900

    feat: 사용자 정보 수정 요청 DTO 구현

---
+++ src/main/java/com/vidcentral/api/dto/request/UpdateMemberRequest.java
@@ -0,0 +1,18 @@
+package com.vidcentral.api.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record UpdateMemberRequest(
+	@NotBlank(message = "닉네임은 필수 입력 항목입니다.")
+	@Size(min = 3, max = 20, message = "닉네임은 3자 이상 20자 이하로 설정해야 합니다.")
+	String nickname,
+
+	@Size(max = 50, message = "소개글은 최대 50자까지 입력이 가능합니다.")
+	String introduce,
+
+	String profileImageURL
+) {
+}
