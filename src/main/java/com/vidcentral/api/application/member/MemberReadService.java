dd8a7551c76328f494a01e04cc4fdb7333de6873
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 15:23:12 +0900

    refactor: 반환 타입 수정 및 에러 메시지 정의

--- src/main/java/com/vidcentral/api/application/member/MemberReadService.java
+++ src/main/java/com/vidcentral/api/application/member/MemberReadService.java
@@ -1,11 +1,11 @@
 package com.vidcentral.api.application.member;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
 import com.vidcentral.api.domain.member.entity.Member;
 import com.vidcentral.api.domain.member.repository.MemberRepository;
+import com.vidcentral.global.error.exception.NotFoundException;
+import com.vidcentral.global.error.model.ErrorMessage;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,12 +15,13 @@ public class MemberReadService {
 
 	private final MemberRepository memberRepository;
 
-	public Member readMember(String email) {
-		return memberRepository.findMemberByEmail(email)
-			.orElseThrow(() -> new IllegalArgumentException("[❎ ERROR] 요청하신 사용자를 찾을 수 없습니다."));
+	public Member readMember(Long memberId) {
+		return memberRepository.findById(memberId)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.FAILED_MEMBER_NOT_FOUND));
 	}
 
-	public Optional<Member> findMember(String email) {
-		return memberRepository.findMemberByEmail(email);
+	public Member findMember(String email) {
+		return memberRepository.findMemberByEmail(email)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.FAILED_MEMBER_NOT_FOUND));
 	}
 }
