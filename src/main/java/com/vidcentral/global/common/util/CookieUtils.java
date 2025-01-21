91e66f29a9b4fd1867ce445250e4967549c75088
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 13:02:26 +0900

    feat: 쿠키 생성 및 토큰 추출 로직 구현

---
+++ src/main/java/com/vidcentral/global/common/util/CookieUtils.java
@@ -0,0 +1,40 @@
+package com.vidcentral.global.common.util;
+
+import static com.vidcentral.global.common.util.TokenConstant.*;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class CookieUtils {
+
+	private static final int COOKIE_MAX_AGE = 24 * 60 * 60;
+
+	public static Cookie generateRefreshTokenCookie(String refreshTokenName, String token) {
+		Cookie refreshTokenCookie = new Cookie(refreshTokenName, token);
+		refreshTokenCookie.setMaxAge(COOKIE_MAX_AGE);
+		refreshTokenCookie.setHttpOnly(true);
+		refreshTokenCookie.setPath("/");
+
+		return refreshTokenCookie;
+	}
+
+	public static String extractRefreshTokenFromCookies(HttpServletRequest httpServletRequest) {
+		if (httpServletRequest.getCookies() != null) {
+			for (Cookie cookie : httpServletRequest.getCookies()) {
+				if (REFRESH_TOKEN_COOKIE.equals(cookie.getName()))
+					return cookie.getValue();
+			}
+		}
+
+		return null;
+	}
+
+	public static Cookie expireRefreshTokenCookie(String refreshTokenName) {
+		Cookie refreshTokenCookie = new Cookie(refreshTokenName, null);
+		refreshTokenCookie.setMaxAge(0);
+		refreshTokenCookie.setHttpOnly(true);
+		refreshTokenCookie.setPath("/");
+
+		return refreshTokenCookie;
+	}
+}
