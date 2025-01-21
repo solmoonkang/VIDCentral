790f25855be2e6f81fa385e669844cc5003d3bc6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 14:38:37 +0900

    feat: 비디오 모델 생성

---
+++ src/main/java/com/vidcentral/api/domain/video/entity/Video.java
@@ -0,0 +1,51 @@
+package com.vidcentral.api.domain.video.entity;
+
+import com.vidcentral.api.domain.member.entity.Member;
+import com.vidcentral.global.common.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "VIDEO")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Video extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "video_id")
+	private Long videoId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	@Column(name = "file_path", nullable = false)
+	private String filePath;
+
+	@Builder
+	private Video(Member member, String title, String description, String filePath) {
+		this.member = member;
+		this.title = title;
+		this.description = description;
+		this.filePath = filePath;
+	}
+}
