14f05e99910c47ccefda45053da9664c9e21fe48
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 14:26:39 +0900

    feat: 전역 예외 처리 핸들러 구현

---
+++ src/main/java/com/vidcentral/global/error/handler/GlobalExceptionHandler.java
@@ -0,0 +1,64 @@
+package com.vidcentral.global.error.handler;
+
+import static com.vidcentral.global.error.model.ErrorMessage.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.vidcentral.global.error.exception.BadRequestException;
+import com.vidcentral.global.error.exception.ConflictException;
+import com.vidcentral.global.error.exception.NotFoundException;
+import com.vidcentral.global.error.exception.VIDCentralException;
+import com.vidcentral.global.error.model.ErrorResponse;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GlobalExceptionHandler {
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	protected ErrorResponse handleException() {
+		log.error("[✅ LOGGER] GLOBAL EXCEPTION HANDLER: 알 수 없는 에러");
+
+		return new ErrorResponse(FAILED_UNKNOWN_ERROR.getMessage(), null);
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(VIDCentralException.class)
+	protected ErrorResponse handleAuthPlaygroundException(VIDCentralException vidCentralException) {
+		log.error("[✅ LOGGER] GLOBAL EXCEPTION HANDLER: 서버 에러");
+
+		return new ErrorResponse(vidCentralException.getMessage(), null);
+	}
+
+	@ResponseStatus(HttpStatus.CONFLICT)
+	@ExceptionHandler(ConflictException.class)
+	protected ErrorResponse handleConflictException(ConflictException conflictException) {
+		log.error("[✅ LOGGER] GLOBAL EXCEPTION HANDLER: 충돌 에러");
+
+		return new ErrorResponse(conflictException.getMessage(), null);
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	protected ErrorResponse handleNotFoundException(NotFoundException notFoundException) {
+		log.error("[✅ LOGGER] GLOBAL EXCEPTION HANDLER: 리소스를 찾을 수 없는 에러");
+
+		return new ErrorResponse(notFoundException.getMessage(), null);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BadRequestException.class)
+	protected ErrorResponse handleException(BadRequestException badRequestException) {
+		log.error("[✅ LOGGER] GLOBAL EXCEPTION HANDLER: 잘못된 요청 에러");
+
+		return new ErrorResponse(badRequestException.getMessage(), null);
+	}
+}
