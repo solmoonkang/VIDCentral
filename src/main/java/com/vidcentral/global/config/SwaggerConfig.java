1aa86a9b5d167b5e7c8b8c2835ae9e4afac50a2b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 13:25:37 +0900

    chore: OpenAPI Swagger 설정 클래스 구현

---
+++ src/main/java/com/vidcentral/global/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.vidcentral.global.config;
+
+import static com.vidcentral.global.common.util.TokenConstant.*;
+import static io.swagger.v3.oas.models.security.SecurityScheme.In.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+
+@Configuration
+public class SwaggerConfig {
+
+	private static final String OPEN_API_TITLE = "Video Platform API Documentation";
+	private static final String OPEN_API_DESCRIPTION = "해당 API는 사용자 인증, 비디오 업로드, 댓글 기능 등을 제공합니다.";
+	private static final String SECURITY_SCHEME_DESCRIPTION = "JWT 인증을 위한 BEARER 토큰입니다.";
+	private static final String VERSION = "1.0.0";
+
+	@Bean
+	public OpenAPI swaggerOpenAPI() {
+		final Info info = new Info().title(OPEN_API_TITLE).description(OPEN_API_DESCRIPTION).version(VERSION);
+		final SecurityRequirement securityRequirement = new SecurityRequirement().addList(ACCESS_TOKEN_HEADER);
+		final SecurityScheme accessTokenSecurityScheme = getSecurityScheme();
+		final Components components = new Components().addSecuritySchemes(BEARER, accessTokenSecurityScheme);
+
+		return new OpenAPI().info(info).addSecurityItem(securityRequirement).components(components);
+	}
+
+	private SecurityScheme getSecurityScheme() {
+		return new SecurityScheme()
+			.name(ACCESS_TOKEN_HEADER)
+			.type(Type.HTTP)
+			.scheme(BEARER)
+			.in(HEADER)
+			.description(SECURITY_SCHEME_DESCRIPTION);
+	}
+}
