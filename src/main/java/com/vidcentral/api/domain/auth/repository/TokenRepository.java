e0a8493d2c70630952e8d92be42034367d4d6c88
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 19:56:32 +0900

    feat: 토큰 정보 관리를 위한 저장소 구현

---
+++ src/main/java/com/vidcentral/api/domain/auth/repository/TokenRepository.java
@@ -0,0 +1,32 @@
+package com.vidcentral.api.domain.auth.repository;
+
+import static com.vidcentral.global.common.util.RedisConstant.*;
+import static com.vidcentral.global.common.util.TokenConstant.*;
+
+import java.time.Duration;
+
+import org.springframework.stereotype.Repository;
+
+import com.vidcentral.api.dto.response.auth.TokenSaveResponse;
+import com.vidcentral.api.infrastructure.redis.HashRedisRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class TokenRepository {
+
+	private final HashRedisRepository hashRedisRepository;
+
+	public void saveToken(String email, TokenSaveResponse tokenSaveResponse) {
+		hashRedisRepository.save(REDIS_REFRESH_TOKEN_PREFIX + email, tokenSaveResponse, Duration.ofDays(EXPIRE_DAYS));
+	}
+
+	public TokenSaveResponse getTokenSaveValue(String email) {
+		return (TokenSaveResponse)hashRedisRepository.get(REDIS_REFRESH_TOKEN_PREFIX + email);
+	}
+
+	public void deleteToken(String email) {
+		hashRedisRepository.delete(REDIS_REFRESH_TOKEN_PREFIX + email);
+	}
+}
