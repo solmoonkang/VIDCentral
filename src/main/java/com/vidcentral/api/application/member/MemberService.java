4f0a409a9fec2493d0cd7656e6132d5bd91c4fa1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 16:22:41 +0900

    feat: 사용자 정보 수정 서비스 로직 구현

--- src/main/java/com/vidcentral/api/application/member/MemberService.java
+++ src/main/java/com/vidcentral/api/application/member/MemberService.java
@@ -5,10 +5,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.vidcentral.api.application.auth.AuthenticationMapper;
 import com.vidcentral.api.application.auth.AuthorizationService;
+import com.vidcentral.api.domain.auth.entity.AuthMember;
 import com.vidcentral.api.domain.member.entity.Member;
 import com.vidcentral.api.dto.request.LoginRequest;
 import com.vidcentral.api.dto.request.SignUpRequest;
 import com.vidcentral.api.dto.request.TokenRequest;
+import com.vidcentral.api.dto.request.UpdateMemberRequest;
 import com.vidcentral.api.dto.response.LoginResponse;
 import com.vidcentral.api.dto.response.MemberInfoResponse;
 
@@ -25,12 +27,17 @@ public class MemberService {
 
 	@Transactional
 	public void signUpMember(SignUpRequest signUpRequest) {
+		memberReadService.validateEmailDuplication(signUpRequest.email());
+		memberReadService.validateNicknameDuplication(signUpRequest.nickname());
+		memberReadService.validatePasswordMatch(signUpRequest.password(), signUpRequest.checkPassword());
 		memberWriteService.signUpMember(signUpRequest);
 	}
 
 	@Transactional
 	public LoginResponse loginMember(LoginRequest loginRequest) {
 		final Member loginMember = memberReadService.findMember(loginRequest.email());
+		memberReadService.validateLoginPasswordMatch(loginRequest.password(), loginMember.getPassword());
+
 		TokenRequest tokenRequest = authorizationService
 			.issueServiceToken(loginMember.getEmail(), loginMember.getNickname());
 		loginMember.updateRefreshToken(tokenRequest.refreshToken());
@@ -41,5 +48,14 @@ public class MemberService {
 	public MemberInfoResponse searchMemberInfo(Long memberId) {
 		final Member loginMember = memberReadService.readMember(memberId);
 		return MemberMapper.toMemberInfoResponse(loginMember);
+	}
+
+	@Transactional
+	public void updateMemberInfo(AuthMember authMember, UpdateMemberRequest updateMemberRequest) {
+		final Member loginMember = memberReadService.findMember(authMember.email());
+		memberReadService.validateNicknameDuplication(updateMemberRequest.nickname());
+		memberReadService.validateProfileImageURLExtension(updateMemberRequest.profileImageURL());
+
+		memberWriteService.updateMemberInfo(loginMember, updateMemberRequest);
 	}
 }
