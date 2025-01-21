6a481b3f8f8b873c527874e7e670e2a16a59dafe
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 14:38:42 +0900

    feat: 댓글 모델 생성

---
+++ src/main/java/com/vidcentral/api/domain/comment/entity/Comment.java
@@ -0,0 +1,49 @@
+package com.vidcentral.api.domain.comment.entity;
+
+import com.vidcentral.api.domain.member.entity.Member;
+import com.vidcentral.api.domain.video.entity.Video;
+import com.vidcentral.global.common.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "COMMENT")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "comment_id")
+	private Long commentId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "video_id", nullable = false)
+	private Video video;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Column(name = "content", nullable = false)
+	private String content;
+
+	@Builder
+	private Comment(Video video, Member member, String content) {
+		this.video = video;
+		this.member = member;
+		this.content = content;
+	}
+}
