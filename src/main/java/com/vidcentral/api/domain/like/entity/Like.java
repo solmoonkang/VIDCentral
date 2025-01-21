b5f587521175d4434947673043c73769cd7cbe41
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 14:38:46 +0900

    feat: 좋아요 모델 생성

---
+++ src/main/java/com/vidcentral/api/domain/like/entity/Like.java
@@ -0,0 +1,49 @@
+package com.vidcentral.api.domain.like.entity;
+
+import com.vidcentral.api.domain.member.entity.Member;
+import com.vidcentral.api.domain.video.entity.Video;
+import com.vidcentral.global.common.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "LIKE")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "like_id")
+	private Long likeId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "video_id", nullable = false)
+	private Video video;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Column(name = "is_like", nullable = false)
+	private Boolean isLike;
+
+	@Builder
+	private Like(Video video, Member member, Boolean isLike) {
+		this.video = video;
+		this.member = member;
+		this.isLike = isLike;
+	}
+}
