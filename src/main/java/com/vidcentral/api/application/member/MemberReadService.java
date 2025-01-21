7a2686d05bae4899cd34c1742263993e482c4740
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 16:21:18 +0900

    refactor: 회원 정보 검증 로직 분리

--- src/main/java/com/vidcentral/api/application/member/MemberReadService.java
+++ src/main/java/com/vidcentral/api/application/member/MemberReadService.java
@@ -1,9 +1,15 @@
 package com.vidcentral.api.application.member;
 
+import static com.vidcentral.global.common.util.ImageURL.*;
+import static com.vidcentral.global.error.model.ErrorMessage.*;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.vidcentral.api.domain.member.entity.Member;
 import com.vidcentral.api.domain.member.repository.MemberRepository;
+import com.vidcentral.global.error.exception.BadRequestException;
+import com.vidcentral.global.error.exception.ConflictException;
 import com.vidcentral.global.error.exception.NotFoundException;
 import com.vidcentral.global.error.model.ErrorMessage;
 
@@ -13,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberReadService {
 
+	private final PasswordEncoder passwordEncoder;
 	private final MemberRepository memberRepository;
 
 	public Member readMember(Long memberId) {
@@ -23,5 +30,37 @@ public class MemberReadService {
 	public Member findMember(String email) {
 		return memberRepository.findMemberByEmail(email)
 			.orElseThrow(() -> new NotFoundException(ErrorMessage.FAILED_MEMBER_NOT_FOUND));
+	}
+
+	public void validateEmailDuplication(String email) {
+		if (memberRepository.existsMemberByEmail(email)) {
+			throw new ConflictException(FAILED_EMAIL_DUPLICATION);
+		}
+	}
+
+	public void validateNicknameDuplication(String nickname) {
+		if (memberRepository.existsMemberByNickname(nickname)) {
+			throw new ConflictException(FAILED_NICKNAME_DUPLICATION);
+		}
+	}
+
+	public void validatePasswordMatch(String password, String checkPassword) {
+		if (!password.equals(checkPassword)) {
+			throw new ConflictException(FAILED_PASSWORD_MISMATCH);
+		}
+	}
+
+	public void validateLoginPasswordMatch(String rawPassword, String encodedPassword) {
+		if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
+			throw new BadRequestException(FAILED_INVALID_PASSWORD);
+		}
+	}
+
+	public void validateProfileImageURLExtension(String profileImageURL) {
+		if (profileImageURL == null || (!profileImageURL.toLowerCase().endsWith(JPG) && !profileImageURL.toLowerCase()
+			.endsWith(JPEG) && !profileImageURL.toLowerCase().endsWith(PNG))) {
+
+			throw new ConflictException(FAILED_INVALID_IMAGE_EXTENSION);
+		}
 	}
 }
