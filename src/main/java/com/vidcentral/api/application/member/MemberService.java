20c3ce413c5d01902f41919d22653b9e3f9f975c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-21 13:00:46 +0900

    feat: 사용자 로그인 서비스 로직 구현

--- src/main/java/com/vidcentral/api/application/member/MemberService.java
+++ src/main/java/com/vidcentral/api/application/member/MemberService.java
@@ -3,7 +3,13 @@ package com.vidcentral.api.application.member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.vidcentral.api.application.auth.AuthenticationMapper;
+import com.vidcentral.api.application.auth.AuthorizationService;
+import com.vidcentral.api.domain.member.entity.Member;
+import com.vidcentral.api.dto.request.LoginRequest;
 import com.vidcentral.api.dto.request.SignUpRequest;
+import com.vidcentral.api.dto.request.TokenRequest;
+import com.vidcentral.api.dto.response.LoginResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,9 +19,20 @@ import lombok.RequiredArgsConstructor;
 public class MemberService {
 
 	private final MemberWriteService memberWriteService;
+	private final MemberReadService memberReadService;
+	private final AuthorizationService authorizationService;
 
 	@Transactional
 	public void signUpMember(SignUpRequest signUpRequest) {
 		memberWriteService.signUpMember(signUpRequest);
+	}
+
+	@Transactional
+	public LoginResponse loginMember(LoginRequest loginRequest) {
+		final Member loginMember = memberReadService.readMember(loginRequest.email());
+		TokenRequest tokenRequest = authorizationService.issueServiceToken(loginMember.getEmail(), loginMember.getNickname());
+		loginMember.updateRefreshToken(tokenRequest.refreshToken());
+
+		return AuthenticationMapper.toLoginResponse(tokenRequest.accessToken(), tokenRequest.refreshToken());
 	}
 }
