89b646f8197dc42203b94a97727e5a5e32a2ff01
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-25 22:24:43 +0900

    refactor: 이미지 확장자 상수 수정

--- src/main/java/com/vidcentral/global/common/util/ImageURL.java
+++ src/main/java/com/vidcentral/global/common/util/ImageURL.java
@@ -6,10 +6,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ImageURL {
 
-	public static final String JPG = ".jpg";
-	public static final String JPEG = ".jpeg";
-	public static final String PNG = ".png";
+	public static final String JPG = "jpg";
+	public static final String JPEG = "jpeg";
+	public static final String PNG = "png";
 
 	public static final String DEFAULT_IMAGE_DOMAIN = "https://image.vidcentral.com/";
 	public static final String MEMBER_PROFILE_URL = "default/member-profile.png";
+	public static final String IMAGE_EXTENSION = ".png";
 }
