3103b1edb19d5b03d841318b14576c37a0a1c6c8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-26 23:10:52 +0900

    feat: 비디오 파일 이름 생성 로직 구현

---
+++ src/main/java/com/vidcentral/api/domain/video/entity/VideoName.java
@@ -0,0 +1,27 @@
+package com.vidcentral.api.domain.video.entity;
+
+import static com.vidcentral.global.common.util.GlobalConstant.*;
+
+import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class VideoName {
+
+	private static final String VIDEO_PATH = "video" + DELIMITER;
+
+	private final String fileName;
+
+	public static VideoName createFromMultipartFile(MultipartFile multipartFile, VideoProperties videoProperties) {
+		return switch (videoProperties) {
+			case DEFAULT_VIDEO ->
+				new VideoName(VIDEO_PATH + multipartFile.getName() + "_" + UUID.randomUUID() + VIDEO_EXTENSION);
+		};
+	}
+}
