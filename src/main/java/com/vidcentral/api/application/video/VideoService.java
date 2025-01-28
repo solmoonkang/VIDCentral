a1e96810618c135a7feb5e8a494c370498641152
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-28 21:53:54 +0900

    feat: 비디오 삭제 서비스 로직 구현

--- src/main/java/com/vidcentral/api/application/video/VideoService.java
+++ src/main/java/com/vidcentral/api/application/video/VideoService.java
@@ -25,6 +25,7 @@ public class VideoService {
 	private final MediaService mediaService;
 	private final MemberReadService memberReadService;
 	private final VideoWriteService videoWriteService;
+	private final VideoReadService videoReadService;
 	private final VideoRepository videoRepository;
 
 	@Transactional
@@ -37,11 +38,23 @@ public class VideoService {
 	}
 
 	@Transactional
-	public void updateVideo(AuthMember authMember, UpdateVideoRequest updateVideoRequest, MultipartFile videoURL) {
+	public void updateVideo(AuthMember authMember, Long videoId, UpdateVideoRequest updateVideoRequest,
+		MultipartFile videoURL) {
 		final Member loginMember = memberReadService.findMember(authMember.email());
-		final Video video = VideoMapper.toVideo(loginMember, updateVideoRequest, videoURL.getOriginalFilename());
+		final Video video = videoReadService.findVideo(videoId);
+		videoReadService.validateMemberHasAccess(video.getMember().getEmail(), loginMember.getEmail());
 
 		final String newVideoURL = mediaService.uploadVideo(videoURL, DEFAULT_VIDEO);
 		videoWriteService.updateVideo(video, updateVideoRequest, newVideoURL);
+	}
+
+	@Transactional
+	public void deleteVideo(AuthMember authMember, Long videoId) {
+		final Member loginMember = memberReadService.findMember(authMember.email());
+		final Video video = videoReadService.findVideo(videoId);
+		videoReadService.validateMemberHasAccess(video.getMember().getEmail(), loginMember.getEmail());
+
+		mediaService.deleteVideo(video.getVideoURL());
+		videoRepository.delete(video);
 	}
 }
