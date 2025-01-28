8e44ee32da2221622fb216b87524b5a2ff7b958f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-28 21:21:28 +0900

    feat: 비디오 정보 수정 로직 구현

---
+++ src/main/java/com/vidcentral/api/application/video/VideoWriteService.java
@@ -0,0 +1,19 @@
+package com.vidcentral.api.application.video;
+
+import org.springframework.stereotype.Service;
+
+import com.vidcentral.api.domain.video.entity.Video;
+import com.vidcentral.api.dto.request.video.UpdateVideoRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class VideoWriteService {
+
+	public void updateVideo(Video video, UpdateVideoRequest updateVideoRequest, String newVideoURL) {
+		video.updateTitle(updateVideoRequest.title());
+		video.updateDescription(updateVideoRequest.description());
+		video.updateVideoURL(newVideoURL);
+	}
+}
