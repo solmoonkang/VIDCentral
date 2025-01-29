d90d0cdcb6da824597582c045be9c213b6bf7a5c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-29 18:48:40 +0900

    feat: 시청 기록 저장소 구현

---
+++ src/main/java/com/vidcentral/api/domain/viewHistory/repository/ViewHistoryRepository.java
@@ -0,0 +1,15 @@
+package com.vidcentral.api.domain.viewHistory.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vidcentral.api.domain.member.entity.Member;
+import com.vidcentral.api.domain.viewHistory.entity.ViewHistory;
+
+@Repository
+public interface ViewHistoryRepository extends JpaRepository<ViewHistory, Long> {
+
+	List<ViewHistory> findViewHistoriesByMember(Member member);
+}
