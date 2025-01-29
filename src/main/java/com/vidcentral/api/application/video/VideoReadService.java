30effa64339a580431f0577b5d977c94a9c9c03b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-29 15:30:18 +0900

    feat: 비디오 태그 개수 제한 검증 로직 구현

--- src/main/java/com/vidcentral/api/application/video/VideoReadService.java
+++ src/main/java/com/vidcentral/api/application/video/VideoReadService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.vidcentral.api.domain.video.entity.Video;
+import com.vidcentral.api.domain.video.entity.VideoTag;
 import com.vidcentral.api.domain.video.repository.VideoRepository;
 import com.vidcentral.global.error.exception.BadRequestException;
 import com.vidcentral.global.error.exception.NotFoundException;
@@ -16,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class VideoReadService {
+
+	private static final int MAX_TAG_COUNT = 3;
 
 	private final VideoRepository videoRepository;
 
@@ -31,6 +34,12 @@ public class VideoReadService {
 	public void validateMemberHasAccess(String videoOwnerEmail, String authMemberEmail) {
 		if (!videoOwnerEmail.equals(authMemberEmail)) {
 			throw new BadRequestException(FAILED_INVALID_VIDEO_ACCESS_ERROR);
+		}
+	}
+
+	public void validateTagCount(List<VideoTag> videoTags) {
+		if (videoTags == null || videoTags.size() > MAX_TAG_COUNT) {
+			throw new BadRequestException(FAILED_MAX_TAG_COUNT_ERROR);
 		}
 	}
 }
