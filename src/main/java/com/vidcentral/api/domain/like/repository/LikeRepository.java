58758b73f5ed839eed87f0b13efba8946f8c9554
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-30 22:00:39 +0900

    feat: 비디오와 회원을 통해 좋아요 조회 로직 구현

--- src/main/java/com/vidcentral/api/domain/like/repository/LikeRepository.java
+++ src/main/java/com/vidcentral/api/domain/like/repository/LikeRepository.java
@@ -1,10 +1,16 @@
 package com.vidcentral.api.domain.like.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.vidcentral.api.domain.like.entity.Like;
+import com.vidcentral.api.domain.member.entity.Member;
+import com.vidcentral.api.domain.video.entity.Video;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+
+	Optional<Like> findLikeByVideoAndMember(Video video, Member member);
 }
