ffd7649a445a68be779eda08d87f423c5e71fadc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-02 19:30:34 +0900

    feat: 비디오를 통해 댓글 리스트를 조회하는 로직 구현

--- src/main/java/com/vidcentral/api/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/vidcentral/api/domain/comment/repository/CommentRepository.java
@@ -1,10 +1,15 @@
 package com.vidcentral.api.domain.comment.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.vidcentral.api.domain.comment.entity.Comment;
+import com.vidcentral.api.domain.video.entity.Video;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+	List<Comment> findCommentsByVideo(Video video);
 }
