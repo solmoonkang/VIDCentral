4adbae6faf4cd93c878d1a864f3b3efbbf103230
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-02 19:31:01 +0900

    feat: 댓글 리스트 조회 서비스 로직 구현

--- src/main/java/com/vidcentral/api/application/comment/CommentService.java
+++ src/main/java/com/vidcentral/api/application/comment/CommentService.java
@@ -1,5 +1,7 @@
 package com.vidcentral.api.application.comment;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,7 @@ import com.vidcentral.api.domain.comment.repository.CommentRepository;
 import com.vidcentral.api.domain.member.entity.Member;
 import com.vidcentral.api.domain.video.entity.Video;
 import com.vidcentral.api.dto.request.comment.UploadCommentRequest;
+import com.vidcentral.api.dto.response.comment.CommentListResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,10 +28,19 @@ public class CommentService {
 
 	@Transactional
 	public void uploadComment(AuthMember authMember, Long videoId, UploadCommentRequest uploadCommentRequest) {
-		final Member member = memberReadService.findMember(authMember.email());
+		final Member loginMember = memberReadService.findMember(authMember.email());
 		final Video video = videoReadService.findVideo(videoId);
 
-		final Comment comment = CommentMapper.toComment(member, video, uploadCommentRequest.content());
+		final Comment comment = CommentMapper.toComment(loginMember, video, uploadCommentRequest.content());
 		commentRepository.save(comment);
+	}
+
+	public List<CommentListResponse> searchAllComments(Long videoId) {
+		final Video video = videoReadService.findVideo(videoId);
+		final List<Comment> comments = commentRepository.findCommentsByVideo(video);
+
+		return comments.stream()
+			.map(CommentMapper::toCommentListResponse)
+			.toList();
 	}
 }
