f4bcacdf41dc1fe52d20c89baa41ace0d5de9b83
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-03 20:08:01 +0900

    feat: 비디오 조회 수 KEY 정의

--- src/main/java/com/vidcentral/global/common/util/RedisConstant.java
+++ src/main/java/com/vidcentral/global/common/util/RedisConstant.java
@@ -6,5 +6,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RedisConstant {
 
-	public static final String REDIS_REFRESH_TOKEN_PREFIX = "refreshToken";
+	public static final String REDIS_REFRESH_TOKEN_PREFIX = "refreshToken:";
+	public static final String REDIS_VIDEO_VIEW_PREFIX = "videoView:";
+
+	public static final int TOKEN_EXPIRE_DAYS = 14;
+	public static final int VIEWS_EXPIRE_MIN = 5;
 }
