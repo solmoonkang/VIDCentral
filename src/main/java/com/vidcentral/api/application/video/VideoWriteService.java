d199ae67c765e50129dd7f81dd6a04a2437e572e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-03 20:10:38 +0900

    feat: 비디오 조회 수 비동기 처리 및 스케줄러 처리

--- src/main/java/com/vidcentral/api/application/video/VideoWriteService.java
+++ src/main/java/com/vidcentral/api/application/video/VideoWriteService.java
@@ -1,18 +1,32 @@
 package com.vidcentral.api.application.video;
 
+import static com.vidcentral.global.common.util.GlobalConstant.*;
+import static com.vidcentral.global.common.util.RedisConstant.*;
+import static com.vidcentral.global.error.model.ErrorMessage.*;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.vidcentral.api.domain.video.entity.Video;
+import com.vidcentral.api.domain.video.repository.VideoManageRepository;
 import com.vidcentral.api.domain.video.repository.VideoRepository;
 import com.vidcentral.api.dto.request.video.UpdateVideoRequest;
+import com.vidcentral.global.error.exception.NotFoundException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class VideoWriteService {
 
 	private final VideoRepository videoRepository;
+	private final VideoManageRepository videoManageRepository;
 
 	public Video saveVideo(Video video) {
 		return videoRepository.save(video);
@@ -29,7 +43,30 @@ public class VideoWriteService {
 		videoRepository.delete(video);
 	}
 
-	public void incrementVideoViews(Video video) {
-		video.incrementViews();
+	@Async
+	public void incrementVideoViewsAsync(Long videoId) {
+		CompletableFuture.runAsync(() -> {
+			videoManageRepository.incrementViews(videoId);
+		}).thenAccept(success -> {
+			log.info("[✅ LOGGER] 조회 수 증가가 완료되었습니다.{}", videoId);
+		}).exceptionally(exception -> {
+			log.warn("[❎ LOGGER] 조회 수 증가 중 오류가 발생했습니다.{}", exception.getMessage());
+			throw new NotFoundException(FAILED_VIDEO_NOT_FOUND_ERROR);
+		});
+	}
+
+	@Scheduled(fixedRate = 600000)
+	public void syncVideoViewsFromRedisToMySQL() {
+		Set<String> videoKeys = videoManageRepository.findAllKeys(REDIS_VIDEO_VIEW_PREFIX + WILDCARD);
+
+		for (String key : videoKeys) {
+			final Long videoId = Long.parseLong(key.split(COLON_DELIMITER)[1]);
+			final Long views = videoManageRepository.findViews(videoId);
+
+			if (views != null) {
+				videoRepository.incrementViews(videoId, views);
+				videoManageRepository.deleteViews(videoId);
+			}
+		}
 	}
 }
