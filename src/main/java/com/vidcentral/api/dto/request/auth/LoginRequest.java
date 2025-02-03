bd59a8864ed1c2479e54da88b1fe0083b684129d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-03 15:36:24 +0900

    chore: 요청 DTO API 문서화

--- src/main/java/com/vidcentral/api/dto/request/auth/LoginRequest.java
+++ src/main/java/com/vidcentral/api/dto/request/auth/LoginRequest.java
@@ -1,10 +1,18 @@
 package com.vidcentral.api.dto.request.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "사용자 로그인 요청")
 public record LoginRequest(
+	@NotBlank(message = "이메일은 필수 입력 항목입니다.")
+	@Schema(description = "사용자 이메일", example = "memberEmail@example.com")
 	String email,
+
+	@NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+	@Schema(description = "사용자 비밀번호", example = "memberPassword123")
 	String password
 ) {
 }
--- src/main/java/com/vidcentral/api/dto/request/member/UpdateMemberRequest.java
+++ src/main/java/com/vidcentral/api/dto/request/member/UpdateMemberRequest.java
@@ -1,11 +1,13 @@
 package com.vidcentral.api.dto.request.member;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
-public record UpdateRequest(
+@Schema(description = "사용자 정보 수정 요청")
+public record UpdateMemberRequest(
 	@NotBlank(message = "닉네임은 필수 입력 항목입니다.")
 	@Size(min = 3, max = 20, message = "닉네임은 3자 이상 20자 이하로 설정해야 합니다.")
 	String nickname,
