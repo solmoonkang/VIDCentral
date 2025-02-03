cb9551da8e3bee0c58f88c381913eff763175b99
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-03 13:11:54 +0900

    refactor: 반환 타입 변경

--- src/main/java/com/vidcentral/api/domain/video/repository/VideoRepository.java
+++ src/main/java/com/vidcentral/api/domain/video/repository/VideoRepository.java
@@ -1,7 +1,7 @@
 package com.vidcentral.api.domain.video.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +10,7 @@ import com.vidcentral.api.domain.video.entity.Video;
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
 
-	List<Video> findVideosByTitle(String title);
+	Page<Video> findVideosByTitle(String title, Pageable pageable);
 
-	List<Video> findVideosByDescription(String description);
+	Page<Video> findVideosByDescription(String description, Pageable pageable);
 }
