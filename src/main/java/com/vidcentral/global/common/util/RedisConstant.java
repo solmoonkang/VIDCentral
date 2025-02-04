ba508e668c0053cdebf49d4c114e9a8b8e0b47a4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-04 17:39:26 +0900

    feat: 검색 기록 레디스 키 정의

--- src/main/java/com/vidcentral/global/common/util/RedisConstant.java
+++ src/main/java/com/vidcentral/global/common/util/RedisConstant.java
@@ -8,6 +8,7 @@ public class RedisConstant {
 
 	public static final String REDIS_REFRESH_TOKEN_PREFIX = "refreshToken:";
 	public static final String REDIS_VIDEO_VIEW_PREFIX = "videoView:";
+	public static final String REDIS_SEARCH_HISTORY_PREFIX = "searchHistory:";
 
 	public static final int TOKEN_EXPIRE_DAYS = 14;
 	public static final int VIEWS_EXPIRE_MIN = 5;
