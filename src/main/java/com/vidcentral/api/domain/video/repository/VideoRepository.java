ebfcb6c7b01ddbc6bdfb381832c7e6e8ca33d860
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-05 03:00:49 +0900

    refactor: 불필요한 로직 제거

--- src/main/java/com/vidcentral/api/domain/video/repository/VideoRepository.java
+++ src/main/java/com/vidcentral/api/domain/video/repository/VideoRepository.java
@@ -1,7 +1,5 @@
 package com.vidcentral.api.domain.video.repository;
 
-import java.util.Set;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +9,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.vidcentral.api.domain.video.entity.Video;
-import com.vidcentral.api.domain.video.entity.VideoTag;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
@@ -23,12 +20,4 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
 	@Modifying
 	@Query("UPDATE Video V SET V.views = V.views + :views WHERE V.videoId = :videoId")
 	void incrementViews(@Param("videoId") Long videoId, @Param("views") Long views);
-
-	@Query("SELECT DISTINCT V FROM Video V " +
-		"WHERE V.videoTags IN :likedVideoTags OR V.title IN :likedVideoTitles OR V.videoTags IN :viewHistoryVideoTags")
-	Page<Video> findRecommendedVideos(
-		@Param("likedVideoTags") Set<VideoTag> likedVideoTags,
-		@Param("likedVideoTitles") Set<String> likedVideoTitles,
-		@Param("viewHistoryVideoTags") Set<VideoTag> viewHistoryVideoTags,
-		Pageable pageable);
 }
