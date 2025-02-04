b27b2b25ed82a78a915845e2f8c098ae9bf7b7b6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-04 17:23:48 +0900

    refactor: 성능 개선을 위해 쿼리 작성

--- src/main/java/com/vidcentral/api/domain/video/repository/VideoRepository.java
+++ src/main/java/com/vidcentral/api/domain/video/repository/VideoRepository.java
@@ -1,5 +1,7 @@
 package com.vidcentral.api.domain.video.repository;
 
+import java.util.Set;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,15 +11,24 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.vidcentral.api.domain.video.entity.Video;
+import com.vidcentral.api.domain.video.entity.VideoTag;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
 
-	Page<Video> findVideosByTitle(String title, Pageable pageable);
-
-	Page<Video> findVideosByDescription(String description, Pageable pageable);
+	@Query("SELECT DISTINCT V FROM Video V " +
+		"WHERE V.title LIKE %:keyword% OR V.description LIKE %:keyword%")
+	Page<Video> findDistinctVideosByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
 	@Modifying
 	@Query("UPDATE Video V SET V.views = V.views + :views WHERE V.videoId = :videoId")
 	void incrementViews(@Param("videoId") Long videoId, @Param("views") Long views);
+
+	@Query("SELECT DISTINCT V FROM Video V " +
+		"WHERE V.videoTags IN :likedVideoTags OR V.title IN :likedVideoTitles OR V.videoTags IN :viewHistoryVideoTags")
+	Page<Video> findRecommendedVideos(
+		@Param("likedVideoTags") Set<VideoTag> likedVideoTags,
+		@Param("likedVideoTitles") Set<String> likedVideoTitles,
+		@Param("viewHistoryVideoTags") Set<VideoTag> viewHistoryVideoTags,
+		Pageable pageable);
 }
