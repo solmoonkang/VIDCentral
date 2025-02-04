c10063678b0e4ca754259c98b73516d6f5b05891
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-04 17:40:19 +0900

    refactor: 각 역할에 따른 서비스 로직으로 이동

--- src/main/java/com/vidcentral/api/application/history/ViewHistoryService.java
+++ src/main/java/com/vidcentral/api/application/history/ViewHistoryService.java
@@ -1,6 +1,9 @@
 package com.vidcentral.api.application.history;
 
+import static com.vidcentral.global.error.model.ErrorMessage.*;
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +18,7 @@ import com.vidcentral.api.domain.history.entity.ViewHistory;
 import com.vidcentral.api.domain.history.repository.ViewHistoryRepository;
 import com.vidcentral.api.dto.response.page.PageResponse;
 import com.vidcentral.api.dto.response.viewHistory.ViewHistoryListResponse;
+import com.vidcentral.global.error.exception.BadRequestException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +32,12 @@ public class ViewHistoryService {
 	public void saveViewHistory(Member member, Video video) {
 		final ViewHistory viewHistory = ViewHistoryMapper.toViewHistory(member, video);
 		viewHistoryRepository.save(viewHistory);
+	}
+
+	public PageResponse<ViewHistoryListResponse> findAllViewHistory(AuthMember authMember, Pageable pageable) {
+		return Optional.ofNullable(authMember)
+			.map(loginMember -> searchAllViewHistory(loginMember, pageable))
+			.orElseThrow(() -> new BadRequestException(FAILED_INVALID_REQUEST_ERROR));
 	}
 
 	public PageResponse<ViewHistoryListResponse> searchAllViewHistory(AuthMember authMember, Pageable pageable) {
