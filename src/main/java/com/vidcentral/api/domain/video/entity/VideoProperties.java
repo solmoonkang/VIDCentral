4e9d6ab892b80bef3a4622be9b7a05dafea98a57
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-05 20:22:09 +0900

    feat: 기본 비디오 타입 외 다른 타입 추가, 컨텐츠 타입 검증 로직 구현

--- src/main/java/com/vidcentral/api/domain/video/entity/VideoProperties.java
+++ src/main/java/com/vidcentral/api/domain/video/entity/VideoProperties.java
@@ -1,11 +1,15 @@
 package com.vidcentral.api.domain.video.entity;
 
+import java.util.Arrays;
+
 import lombok.Getter;
 
 @Getter
 public enum VideoProperties {
 
-	DEFAULT_VIDEO(100 * 1024 * 1024, "video/mp4");
+	DEFAULT_VIDEO(100 * 1024 * 1024, "video/mp4"),
+	AVI_VIDEO(100 * 1024 * 1024, "video/x-msvideo"),
+	MKV_VIDEO(100 * 1024 * 1024, "video/x-matroska");
 
 	private final long maxSize;
 	private final String contentType;
@@ -13,5 +17,10 @@ public enum VideoProperties {
 	VideoProperties(long maxSize, String contentType) {
 		this.maxSize = maxSize;
 		this.contentType = contentType;
+	}
+
+	public static boolean isSupportedContentType(String contentType) {
+		return Arrays.stream(values())
+			.anyMatch(videoProperties -> videoProperties.contentType.equals(contentType));
 	}
 }
