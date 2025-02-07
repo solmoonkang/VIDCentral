04c87519802ae0412b6481b78be3369907048fcd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-07 22:50:53 +0900

    test: Member 관련 핼퍼 로직 구현

---
+++ src/test/java/com/vidcentral/support/MemberFixture.java
@@ -0,0 +1,48 @@
+package com.vidcentral.support;
+
+import com.vidcentral.api.domain.member.entity.Member;
+import com.vidcentral.api.dto.request.auth.LoginRequest;
+import com.vidcentral.api.dto.request.member.SignUpRequest;
+import com.vidcentral.api.dto.response.auth.LoginResponse;
+import com.vidcentral.api.dto.response.member.MemberInfoResponse;
+
+public class MemberFixture {
+
+	public static Member createMemberEntity() {
+		return Member.builder()
+			.email("testMember@example.com")
+			.password("test123")
+			.nickname("testMemberNickname")
+			.build();
+	}
+
+	public static SignUpRequest createSignUpRequest() {
+		return SignUpRequest.builder()
+			.email("testMember@example.com")
+			.password("test123")
+			.checkPassword("test123")
+			.nickname("testMemberNickname")
+			.build();
+	}
+
+	public static LoginRequest createLoginRequest() {
+		return LoginRequest.builder()
+			.email("testMember@example.com")
+			.password("test123")
+			.build();
+	}
+
+	public static LoginResponse createLoginResponse() {
+		return LoginResponse.builder()
+			.accessToken("testAccessToken")
+			.refreshToken("testRefreshToken")
+			.build();
+	}
+
+	public static MemberInfoResponse createMemberInfoResponse() {
+		return MemberInfoResponse.builder()
+			.nickname("testMemberNickname")
+			.introduce("testIntroduce")
+			.build();
+	}
+}
