1f6269b7ba834e299636f6c6aeb6bd50ec1b38ae
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-07 22:51:09 +0900

    test: 회원가입, 로그인, 회원 목록 조회 로직 테스트 코드 작성

---
+++ src/test/java/com/vidcentral/api/presentation/member/MemberControllerTest.java
@@ -0,0 +1,118 @@
+package com.vidcentral.api.presentation.member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vidcentral.api.application.member.MemberService;
+import com.vidcentral.api.dto.request.auth.LoginRequest;
+import com.vidcentral.api.dto.request.member.SignUpRequest;
+import com.vidcentral.api.dto.response.auth.LoginResponse;
+import com.vidcentral.api.dto.response.member.MemberInfoResponse;
+import com.vidcentral.support.MemberFixture;
+
+@ExtendWith(MockitoExtension.class)
+class MemberControllerTest {
+
+	@InjectMocks
+	private MemberController memberController;
+
+	@Mock
+	private MemberService memberService;
+
+	private MockMvc mockMvc;
+
+	private ObjectMapper objectMapper;
+
+	@BeforeEach
+	public void init() {
+		objectMapper = new ObjectMapper();
+		mockMvc = MockMvcBuilders.standaloneSetup(memberController)
+			.setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
+			.build();
+	}
+
+	@DisplayName("[✅ SUCCESS] signUpMember: 성공적으로 회원가입을 완료했습니다.")
+	@Test
+	void signUpMember_void_success() throws Exception {
+		// GIVEN
+		SignUpRequest signUpRequest = MemberFixture.createSignUpRequest();
+
+		doNothing().when(memberService).signUpMember(any(SignUpRequest.class));
+
+		// WHEN
+		ResultActions resultActions = mockMvc.perform(
+			MockMvcRequestBuilders.post("/api/signup")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(signUpRequest))
+		);
+
+		// THEN
+		MvcResult mvcResult = resultActions.andExpect(status().isOk())
+			.andReturn();
+
+		String content = mvcResult.getResponse().getContentAsString();
+		assertThat(content.replace("\"", "")).isEqualTo("성공적으로 회원가입이 완료되었습니다.");
+	}
+
+	@DisplayName("[✅ SUCCESS] loginMember: 성공적으로 로그인을 완료했습니다.")
+	@Test
+	void loginMember_LoginResponse_success() throws Exception {
+		// GIVEN
+		LoginRequest loginRequest = MemberFixture.createLoginRequest();
+		LoginResponse loginResponse = MemberFixture.createLoginResponse();
+
+		doReturn(loginResponse).when(memberService).loginMember(any(), any(LoginRequest.class));
+
+		// WHEN
+		ResultActions resultActions = mockMvc.perform(
+			MockMvcRequestBuilders.post("/api/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(loginRequest))
+		);
+
+		// THEN
+		resultActions.andExpect(status().isOk())
+			.andExpect(jsonPath("$.accessToken").value(loginResponse.accessToken()))
+			.andExpect(jsonPath("$.refreshToken").value(loginResponse.refreshToken()))
+			.andReturn();
+	}
+
+	@DisplayName("[✅ SUCCESS] searchMemberInfo: 성공적으로 사용자 조회를 완료했습니다.")
+	@Test
+	void searchMemberInfo_MemberInfoResponse_success() throws Exception {
+		// GIVEN
+		Long memberId = 1L;
+		MemberInfoResponse memberInfoResponse = MemberFixture.createMemberInfoResponse();
+
+		doReturn(memberInfoResponse).when(memberService).searchMemberInfo(anyLong());
+
+		// WHEN
+		ResultActions resultActions = mockMvc.perform(
+			MockMvcRequestBuilders.get("/api/members/{memberId}", memberId)
+				.contentType(MediaType.APPLICATION_JSON)
+		);
+
+		// THEN
+		resultActions.andExpect(status().isOk())
+			.andExpect(jsonPath("$.nickname").value(memberInfoResponse.nickname()))
+			.andExpect(jsonPath("$.introduce").value(memberInfoResponse.introduce()))
+			.andReturn();
+	}
+}
