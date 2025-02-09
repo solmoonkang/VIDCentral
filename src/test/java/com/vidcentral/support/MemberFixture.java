947b281a1bf034feb937dc92f452b971b7b47123
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-02-09 20:02:11 +0900

    test: 인증 관련 핼퍼 로직 제거

--- src/test/java/com/vidcentral/support/MemberFixture.java
+++ src/test/java/com/vidcentral/support/MemberFixture.java
@@ -1,10 +1,7 @@
 package com.vidcentral.support;
 
 import com.vidcentral.api.domain.member.entity.Member;
-import com.vidcentral.api.dto.request.auth.LoginRequest;
-import com.vidcentral.api.dto.request.member.SignUpRequest;
 import com.vidcentral.api.dto.request.member.UpdateMemberRequest;
-import com.vidcentral.api.dto.response.auth.LoginResponse;
 import com.vidcentral.api.dto.response.member.MemberInfoResponse;
 
 public class MemberFixture {
@@ -14,33 +11,6 @@ public class MemberFixture {
 			.email("testMember@example.com")
 			.password("test123")
 			.nickname("testMemberNickname")
-			.build();
-	}
-
-	public static SignUpRequest createSignUpRequest() {
-		return SignUpRequest.builder()
-			.email("testMember@example.com")
-			.password("test123")
-			.checkPassword("test123")
-			.nickname("testMemberNickname")
-			.build();
-	}
-
-	public static LoginRequest createLoginRequest() {
-		return LoginRequest.builder()
-			.email("testMember@example.com")
-			.password("test123")
-			.build();
-	}
-
-	public static LoginRequest createLoginRequest(Member member) {
-		return new LoginRequest(member.getEmail(), member.getPassword());
-	}
-
-	public static LoginResponse createLoginResponse() {
-		return LoginResponse.builder()
-			.accessToken("testAccessToken")
-			.refreshToken("testRefreshToken")
 			.build();
 	}
 
